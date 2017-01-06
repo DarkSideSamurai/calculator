0a4cf0e0676479ed01969c54c5f465a64389edde
arya tikku <mac8@129mac18.valleychristian.net>
2017-01-06 14:37:06 -0800

    gwrgdh

--- calculator.js
+++ calculator.js
@@ -1,1 +1,27 @@
 // JavaScript Document
+
+// change display
+
+function d(val){
+document.getElementById("d").value = val;
+}
+
+
+// type values and operators
+
+function v(val){
+document.getElementById("d").value += val;
+}
+
+
+// evaluate
+
+function e(){
+
+try{
+	d(eval(document.getElementById("d").value));
+}
+catch(err) {
+	d("error 404");
+}
+}
