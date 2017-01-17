51a334d47f07fa640e37d44025692756f74dde60
arya tikku <mac8@129mac18.valleychristian.net>
2017-01-17 14:41:02 -0800

    changed stuff
    made buttons bigger
    bug fixes
    added all clear and off

--- calculator.js
+++ calculator.js
@@ -4,9 +4,21 @@
 
 function d(val) {
 	document.getElementById("d").value = val;
+}
 
 // type numbers and operators
 
+function v(val) {
+	document.getElementById("d").value += val;
+}
 
 // evaluate the equation
 
+function e() {
+try {
+	d(eval(document.getElementById("d").value));
+}
+catch(err) {
+	d("error");
+}
+}
